cf8c5d403d7c1735998dc92be381e35d69839c34
chtello <chtello@escuelasproa.edu.ar>
2024-11-19 14:49:30 -0300

    guia empezada

---
+++ gia/ej3.js
@@ -0,0 +1,7 @@
+function fibonacci(n) {
+    if (n = 0) {
+        return 1
+    }else{
+        return 
+    }
+}
---
+++ matriz.js
@@ -0,0 +1,9 @@
+function matrizCuadrada (m,i,j) {
+    if (m) {
+        
+    }
+}
+console.log(matrizCuadrada(
+    [[1,2,3],
+    [1,2,3],
+    [1,2,3]],i,j))
